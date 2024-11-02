b6540a33c9e3e14bbc1b96d12ccc9195db4aaf7a
jayodele64 <jayodele64@gmail.com>
2024-11-02 02:33:33 +0100

    fix build issue

--- mocks/handlers.ts
+++ mocks/handlers.ts
@@ -1,10 +1,10 @@
-// src/mocks/handlers.js
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { http, HttpResponse } from 'msw';
+import environmentVariables from '../src/utils/environmentVariables';
 import mockedAllCoins from './__mock_data__.json';
 
 const handlers = [
-  http.get('/api/tickers/', () => HttpResponse.json(mockedAllCoins)),
+  http.get(`${environmentVariables.baseURL}/api/tickers/`, () => HttpResponse.json(mockedAllCoins)),
 ];
 
 export default handlers;
