53fc5fdf949fb55e9b1485488e80e0be59ac678f
jayodele64 <jayodele64@gmail.com>
2024-11-02 02:27:24 +0100

    fix handler

--- mocks/handlers.ts
+++ mocks/handlers.ts
@@ -5,7 +5,7 @@ import environmentVariables from '../src/utils/environmentVariables';
 import mockedAllCoins from './__mock_data__.json';
 
 const handlers = [
-  http.get(`${environmentVariables.baseURL}/api/tickers/`, () => HttpResponse.json(mockedAllCoins)),
+  http.get(`/api/tickers/`, () => HttpResponse.json(mockedAllCoins)),
 ];
 
 export default handlers;
