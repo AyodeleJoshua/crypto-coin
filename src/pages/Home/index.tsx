e11b8470a1341d99574e451673aeaa1513a55277
jayodele64 <jayodele64@gmail.com>
2024-11-03 01:24:39 +0100

    implement SRP

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,6 @@
-import { useEffect, useState } from 'react';
-import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { Spinner, Table } from '../../components';
-import getAllCoinsData from '../../services/crypto.services';
 import styles from './home.module.css';
+import useCoins from '../../customHooks/useCoins';
 
 const columns = [
   { key: 'name', title: '💰 Coin' },
@@ -11,48 +9,15 @@ const columns = [
   { key: 'tsupply', title: '📈 Total Supply' },
 ];
 
-interface AllCoinsData {
-  data: {
-    symbol: string;
-    name: string;
-    price_usd: string;
-    tsupply: string;
-  }[];
-}
-
 function Home() {
-  const queryClient = useQueryClient();
-  const [queryParams, setQueryParams] = useState({
-    page: 0,
-    pageSize: 10,
-  });
-
-  // This useeffect prefetches the next data so that users do not
-  // see loading state when they go to next page.
-  // User only see loading state once when the page is newly loaded
-  useEffect(() => {
-    queryClient.prefetchQuery({
-      queryKey: ['all-coins', queryParams.page + 1, queryParams.pageSize],
-      queryFn: () => getAllCoinsData(
-        (queryParams.page + 1) * queryParams.pageSize,
-        queryParams.pageSize,
-      ),
-      staleTime: Infinity,
-    });
-  }, [queryParams, queryClient]);
-
   const {
-    isLoading: isLoadingAllCoinsData,
-    data: allCoinsData,
-    isError: isAllCoinsFetchError,
-    error: allCoinsError,
-  } = useQuery<AllCoinsData>({
-    queryFn: () => getAllCoinsData(
-      queryParams.page * queryParams.pageSize,
-      queryParams.pageSize,
-    ),
-    queryKey: ['all-coins', queryParams.page, queryParams.pageSize],
-  });
+    isLoadingAllCoinsData,
+    allCoinsData,
+    queryParams,
+    setQueryParams,
+    isAllCoinsFetchError,
+    allCoinsError,
+  } = useCoins();
 
   return (
     <>
@@ -61,7 +26,7 @@ function Home() {
       {!isLoadingAllCoinsData && allCoinsData && (
         <Table
           columns={columns}
-          dataSource={allCoinsData.data.map((coinData) => ({
+          dataSource={allCoinsData.map((coinData) => ({
             ...coinData,
             tsupply: `${Number(coinData.tsupply).toLocaleString()} ${
               coinData.symbol
@@ -79,7 +44,7 @@ function Home() {
       {!isLoadingAllCoinsData && isAllCoinsFetchError && (
         <div className={styles.errorContainer}>
           <div>
-            <p>{allCoinsError.message}</p>
+            <p>{allCoinsError?.message}</p>
             <p>Try again later</p>
           </div>
         </div>
