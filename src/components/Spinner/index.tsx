68bc158dcc1afbce5c7fe341fe859102024ef87d
jayodele64 <jayodele64@gmail.com>
2024-11-05 23:32:12 +0100

    remove react icons library

--- src/components/Spinner/index.tsx
+++ src/components/Spinner/index.tsx
@@ -1,10 +1,9 @@
-import { ImSpinner2 } from 'react-icons/im';
 import styles from './spinner.module.css';
 
 function Spinner() {
   return (
     <div className={styles.spinnerContainer}>
-      <ImSpinner2 className={styles.spinnerIcon} />
+      <div className={styles.loader} />
     </div>
   );
 }
